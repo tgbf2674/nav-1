63b0ad4e7ef8bc27db6b9bfa78dfc7c957cdf70e
tgbf2674 <tgbf2020@163.com>
2020-11-06 16:25:05 +0800

    添加背景图，取消下拉框

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -207,7 +207,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56266" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50935" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
